38046d76f5bfd018ffe0f8d36a6f8a0d8544d54a
13099406826Ycb <ycb04106438@163.com>
2026-06-25 20:31:47 +0800

    1

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@ export TRANSFORMERS_CACHE=/data1/ycb/cache/huggingface/hub
 export XDG_CACHE_HOME=/data1/ycb/cache/xdg
 export NO_ALBUMENTATIONS_UPDATE=1
 
-CUDA_VISIBLE_DEVICES=6,7 \
+CUDA_VISIBLE_DEVICES=2,3 \
 WANDB_DISABLED=true \
 PYTHONUNBUFFERED=1 \
 uv run --no-sync torchrun --nproc_per_node=2 --master_port=29500 \
@@ -24,7 +24,7 @@ uv run --no-sync torchrun --nproc_per_node=2 --master_port=29500 \
     --dataset-path /data1/ycb/datasets/pick_and_place_apple_right_230/lerobot_v2.1 \
     --embodiment-tag UNITREE_G1_UPPER_BODY \
     --modality-config-path examples/G1/upper_body/unitree_g1_upper_body_config.py \
-    --num-gpus 1 \
+    --num-gpus 2 \
     --output-dir /data1/ycb/checkpoints/GR00T_N1d7_30k_g1_22d_pick_and_place_apple_right_230/ \
     --max-steps 30000 \
     --save-steps 15000 \
